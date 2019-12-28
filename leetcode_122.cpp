8206c6290ebd87920c670ba3381f7c470aea83be
zhs2326 <36891384+zhs2326@users.noreply.github.com>
2019-12-27 22:44:38 -0800

    Update leetcode_122.cpp

--- leetcode_122.cpp
+++ leetcode_122.cpp
@@ -1,10 +1,10 @@
-///
+/*
 The intuition is that every time when you find you can make profits, you should make a transaction.
 Likewise, when you find that the price will go down, you should sell the stock.
 One thing you need to take care of is that in C++, size() will return an unsigned int, which is likely to result in an overflow.
 So I recommend converting the length of the array to int first, namely writing something like: int n = prices.size();.
 And in this way, overflows won't occur.
-///
+*/
 
 class Solution {
 public:
